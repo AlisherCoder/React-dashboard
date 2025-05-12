5caafa03cf72e7fdc2a867bef605894016b5ca2a
alishercoder <alishersharipov670@gmail.com>
2025-05-13 01:39:45 +0500

    update

--- src/pages/dashboard/create/index.jsx
+++ src/pages/dashboard/create/index.jsx
@@ -59,12 +59,12 @@ const Create = () => {
 
                <div>
                   <label className="mb-2 text-sm text-slate-900 font-medium block">Img url</label>
-                  <input required value={url} onChange={(e) => setUrl(e.target.value)} type="text" placeholder="Enter url" className="px-4 py-3 bg-gray-100 focus:bg-transparent w-full text-sm text-slate-900 outline-[#333] rounded-sm transition-all" />
+                  <input value={url} onChange={(e) => setUrl(e.target.value)} type="text" placeholder="Enter url" className="px-4 py-3 bg-gray-100 focus:bg-transparent w-full text-sm text-slate-900 outline-[#333] rounded-sm transition-all" />
                </div>
 
                <div>
                   <label className="mb-2 text-sm text-slate-900 font-medium block">Description</label>
-                  <textarea required value={desc} onChange={(e) => setDesc(e.target.value)} type="text" placeholder="Enter description" className="px-4 py-3 bg-gray-100 focus:bg-transparent w-full text-sm text-slate-900 outline-[#333] rounded-sm transition-all" />
+                  <textarea value={desc} onChange={(e) => setDesc(e.target.value)} type="text" placeholder="Enter description" className="px-4 py-3 bg-gray-100 focus:bg-transparent w-full text-sm text-slate-900 outline-[#333] rounded-sm transition-all" />
                </div>
 
                <button className="!mt-8 px-6 py-2.5 text-sm font-medium bg-[#333] hover:bg-[#222] text-white rounded-sm cursor-pointer">Create</button>
