6413e5943ce55f331eaadcb3b5e1e5a829fff274
alishercoder <alishersharipov670@gmail.com>
2025-05-13 03:21:54 +0500

    update

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import "./style.css";
 
 const Dashboard = () => {
    return (
@@ -8,12 +9,12 @@ const Dashboard = () => {
             <h2 className="text-3xl text-amber-50 mb-8">DashStack</h2>
             <ul className="text-2xl text-amber-50">
                <li className="mb-5">
-                  <NavLink className="bg-blue-600 rounded-[4px] px-2 py-1 cursor-pointer" to={""}>
+                  <NavLink className="block dash_link rounded-[4px] px-2 py-1 cursor-pointer" end={true} to={""}>
                      All products
                   </NavLink>
                </li>
                <li className="mb-5">
-                  <NavLink className="bg-blue-600 rounded-[4px] px-2 py-1 cursor-pointer" to={"create"}>
+                  <NavLink className="block dash_link rounded-[4px] px-2 py-1 cursor-pointer" to={"create"}>
                      Create
                   </NavLink>
                </li>
